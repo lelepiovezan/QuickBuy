ac01d0a0179a29f93fe8ca801abe6f9a5beaa4fc
Leandro Piovezan <leandro.piovezan@yahoo.com.br>
2020-10-24 11:39:17 -0300

    Alteração

---
+++ QuickBuy.Dominio/Entidades/Pedido.cs
@@ -0,0 +1,40 @@
+﻿using QuickBuy.Dominio.ObjetoDeValor;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+   public class Pedido
+    {
+        public int Id { get; set; }
+
+        public DateTime DataPedido{ get; set; }
+
+
+        public int UsuarioId { get; set; }
+
+
+        public DateTime DataPrevisaoEntrega { get; set; }
+
+        public string CEP { get; set; }
+
+        public string Estado { get; set; }
+
+        public int Cidade { get; set; }
+
+        public string EnderecoCompleto { get; set; }
+
+        public int NumeroEndereco { get; set; }
+
+        public int FormaPagamentoId { get; set; }
+        public FormaPagamento FormaPagamento{ get; set; }
+
+        ///<sumary>
+        ///Pedido deve ter pelo menos 1 item pedido ou mutos itens de pedidos
+        ///</sumary>
+
+        public ICollection <ItemPedido>ItemPedido { get; set; }
+
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,61 @@
+﻿using QuickBuy.Dominio.Enumerados;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+   public class FormaPagamento
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public Boolean EhBoleto {
+
+
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        
+        }
+
+        public Boolean EhCartao
+        {
+
+
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+
+        }
+
+
+        public Boolean EhDeposito
+        {
+
+
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+
+        }
+
+
+        public Boolean NaoFoiDefinido
+        {
+
+
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        public object TipoFormaPagamento { get; private set; }
+    }
+}
